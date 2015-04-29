838abbd93bb8db768fea8fa8719ac0062685ba84
ajayhn <ajayhn@yahoo.com>
2015-04-29 16:24:36 -0700

    Update firstboot_compute.sh

--- raw/firstboot_compute.sh
+++ raw/firstboot_compute.sh
@@ -9,6 +9,6 @@ PHYS_INTF=$(grep "^physical_interface=" /etc/contrail/contrail-vrouter-agent.con
 
 dhclient -v ${PHYS_INTF}
 export DHCP_ADDR=$(ifconfig ${PHYS_INTF} | grep "inet addr" | awk '{ print $2 }' | cut -d ':' -f 2)
-sed -i 's/    address.*/    address '${DHCP_ADDR}'/' /etc/network/interfaces
+sed -i 's/    address.*/    address '${DHCP_ADDR}'/' /etc/network/interfaces.d/${PHYS_INTF}.cfg
 sed -i -r -e 's/control_network_ip=.*/control_network_ip='${DHCP_ADDR}'/' -e 's/^ip=.*\/(.*)/ip='${DHCP_ADDR}'\/\1/' /etc/contrail/contrail-vrouter-agent.conf
 touch ${FB_FLAG}
